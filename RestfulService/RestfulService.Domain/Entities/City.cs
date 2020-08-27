0974e1b832dc045fd2d322c69a4f8d61d7a5765e
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-08-27 16:31:48 +0300

    Added Domain layer to store entities

---
+++ RestfulService/RestfulService.Domain/Entities/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestfulService.Domain.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool isCapital { get; set; }
+        public int Population { get; set; } 
+
+        public Country country { get; set; }
+    }
+}
