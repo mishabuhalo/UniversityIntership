f4b4f08f398d90e25602724c35ca5ef1e2589c0e
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-08-28 10:26:22 +0300

    Added controllers

---
+++ RestfulService/RestfulService/Controlers/CitiesController.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestfulService.Application.Cities;
+using RestfulService.Application.Cities.Commands;
+using RestfulService.Application.Cities.Queries;
+using RestfulService.Application.Countries;
+using RestfulService.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RestfulService.Controlers
+{
+    public class CitiesController : ControlerBase
+    {
+        public async Task<IActionResult> List()
+        {
+            return View(await Mediator.Send(new ListCitiesQuery()));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var Countries = await Mediator.Send(new GetCountriesQuery());
+
+            SelectList countries = new SelectList(Countries, "Id", "Name");
+
+            ViewBag.countries = countries;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(City city)
+        {
+            var id = await Mediator.Send(new CreateCityCommand { Name = city.Name, CountryId = city.CountryId, IsCapital = city.isCapital, Population = city.Population });
+            return Redirect($"/Cities/List/{id}");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            await Mediator.Send(new DeleteCityCommand { Id = id.Value });
+            return Redirect("/Cities/List/");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var Countries = await Mediator.Send(new GetCountriesQuery());
+
+            SelectList countries = new SelectList(Countries, "Id", "Name");
+
+            ViewBag.countries = countries;
+            return View(await Mediator.Send(new GetCityByIdQuery { Id = id.Value }));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(City city)
+        {
+            await Mediator.Send(new EditCityCommand { Id = city.Id, Name = city.Name, CountryId = city.CountryId, IsCapital = city.isCapital, Population = city.Population });
+            return Redirect("/Cities/List/");
+        }
+        public async Task<IActionResult> Details(int?id)
+        {
+            
+            return View(await Mediator.Send(new GetCityByIdQuery { Id = id.Value }));
+        }
+    }
+}
---
+++ RestfulService/RestfulService/Controlers/CountriesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestfulService.Application.Countries;
+using RestfulService.Application.Countries.Commands;
+using RestfulService.Domain.Entities;
+using RestfulService.Infrastructure.Persistance;
+
+namespace RestfulService.Controlers
+{
+    public class CountriesController : ControlerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CountriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> List()
+        {
+            return View(await Mediator.Send(new GetCountriesQuery()));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            return View(await Mediator.Send(new GetCountryByIdQuery { ListId = id.Value }));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Country country)
+        {
+            var id = await Mediator.Send(new CreateCountryCommand { Name = country.Name });
+
+            return Redirect($"/Countries/List/{id}");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            return View(await Mediator.Send(new GetCountryByIdQuery { ListId = id.Value }));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Country country)
+        {
+            await Mediator.Send(new EditCountryCommand { Id = country.Id, Name = country.Name });
+            return Redirect("/Countries/List");
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            await Mediator.Send(new DeleteCountryCommand { Id = id.Value });
+            return Redirect("/Countries/List");
+        }
+    }
+}
+
