4162d5713d751c62ade973ae85f6a2a50461f89b
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-08-28 10:25:10 +0300

    Added city queries

---
+++ RestfulService/RestfulService.Application/Cities/Queries/GetCityByIdQuery.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RestfulService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestfulService.Application.Cities.Queries
+{
+    public class GetCityByIdQuery:IRequest<City>
+    {
+        public int Id { get; set; }
+    }
+
+    public class GetCityByIdQueryHandler : IRequestHandler<GetCityByIdQuery, City>
+    {
+        private readonly IApplicationDbContext _context;
+        public GetCityByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<City> Handle(GetCityByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Cities.Where(city => city.Id == request.Id).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ RestfulService/RestfulService.Application/Cities/Queries/ListCitiesQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RestfulService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestfulService.Application.Cities
+{
+    public class ListCitiesQuery : IRequest<List<City>>
+    {
+    }
+
+    public class ListCitiesQueryHandler : IRequestHandler<ListCitiesQuery, List<City>>
+    {
+        private readonly IApplicationDbContext _context;
+        public ListCitiesQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<City>> Handle(ListCitiesQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Cities.ToListAsync();
+        }
+    }
+}
